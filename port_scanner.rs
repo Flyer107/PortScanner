0b52331ed1395033e548f77336f8ed66e3fa5c50
Kwesi <kwesi.b.owubah@gmail.com>
2021-12-29 13:14:27 -0500

    src clean up

--- port_scanner.rs
+++ port_scanner.rs
@@ -7,14 +7,9 @@
 //            port as open.
 // OUTPUT : Status of port (open/closed)
 use std::net::{TcpStream, SocketAddr};
-use std::collections::HashMap;
 use std::io;
 use std::env;
 use std::time::Duration;
-
-//extern crate pnet;
-//use pnet::datalink::{self, NetworkInterface};
-//use pnet::datalink::Channel::Ethernet;
 
 // Main fn to receive ip addr, start port, and end port
 fn main() {
@@ -46,25 +41,9 @@ fn main() {
   let end_port: u16 = args[3].trim().parse::<u16>().unwrap();
 
   let ports_open = scan_range(args[1].trim().to_string().clone(), start_port, end_port);
-  
-  let port_desc: HashMap<u16, &str> = [
-    (20, "ftp"),
-    (21, "ftp"),
-    (22, "ssh"),
-    (23, "telnet"),
-    (50, "IPSec"),
-    (51, "IPSec"),
-    (53, "DNS"),
-    (443, "HTTPS"),
-  ].iter().cloned().collect();
-  //println!("{}",ports_open.len());
 
   for key in ports_open.iter() {
     println!("{} is open", key);
-    
-    // if port_desc.contains_key(&key) {
-    //   println!("{} is open: {:?}", key, port_desc.get_key_value(&key));
-    // }
   }
   
 }
@@ -76,33 +55,9 @@ fn scan_range(addr: std::string::String, start_port: u16, end_port: u16 ) -> Vec
     let server_string = addr.clone() + ":" + curr_port.to_string().as_str().trim();
     let server: SocketAddr = server_string.parse().expect("Unable to parse socket address");
     if let Ok(_) = TcpStream::connect_timeout(&server, Duration::new(0, 500000)) {
-      //println!("Connected to {}", server_string);
       open_ports.push(curr_port);
-    } else {
-      //println!("Couldn't connect to {}", server_string);
-    }
+    } 
   }
   open_ports
   // print from open_ports vector
 }
-
-// Scan within user specified range
-fn scan_range_icmp(addr: std::string::String, start_port: u16, end_port: u16 ) -> Vec<u16> {
-  let mut open_ports = Vec::new();
-  for curr_port in start_port..end_port {
-    let server_string = addr.clone() + ":" + curr_port.to_string().as_str().trim();
-    let server: SocketAddr = server_string.parse().expect("Unable to parse socket address");
-    if let Ok(_) = TcpStream::connect_timeout(&server, Duration::new(0, 500000)) {
-      println!("Connected to {}", server_string);
-      open_ports.push(curr_port);
-    } else {
-      println!("Couldn't connect to {}", server_string);
-    }
-  }
-  open_ports
-  // print from open_ports vector
-}
-
-//172.217.12.228:443
-//https://subinsb.com/default-device-ttl-values/
-//https://docs.rs/ttl_cache/0.5.1/ttl_cache/
